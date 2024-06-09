a44cb41c6233e803b2acff1ae253eb862bbeb387
ptrc01 <111512409+ptrc01@users.noreply.github.com>
2024-06-09 23:10:37 +0700

    add menu to Home.java

--- Home.java
+++ Home.java
@@ -4,5 +4,15 @@ import java.util.Scanner;
 public class MainMenu {
     private static ArrayList<String> usernames = new ArrayList<>();
     private static ArrayList<String> passwords = new ArrayList<>();
-
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Welcome to the User Authentication System");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.print("Please choose an option: ");
+        }
+    }
 }
