069b0f18441e30d171977255daaf8f2832906e5b
ptrc01 <111512409+ptrc01@users.noreply.github.com>
2024-06-11 10:27:45 +0700

    contoh merge conflict

--- Home.java
+++ Home.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainMenu {
-    private static ArrayList<String> usernames = new ArrayList<>();
-    private static ArrayList<String> passwords = new ArrayList<>();
+    private static ArrayList<String> usn = new ArrayList<>();
+    private static ArrayList<String> pw = new ArrayList<>();
 
 }
